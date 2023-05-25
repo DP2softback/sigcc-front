338cf6e3c28185678ff2b5be10570a85abc15b21
FaridFranz <farid@MSI>
2023-05-25 17:36:20 -0500

    linechart y plantillas index

--- dp2-frontend/src/features/Modulo3/screens/Plantillas/Index.tsx
+++ dp2-frontend/src/features/Modulo3/screens/Plantillas/Index.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import Layout from '@features/Modulo3/components/Layout/Content/Content';
+import Section from '@features/Modulo3/components/Layout/Section/Section';
+import { Search } from 'react-bootstrap-icons'
+import { Form, InputGroup, Button } from 'react-bootstrap';
+import BasicCard from '@features/Modulo3/components/Cards/BasicCard/BasicCard';
+import Templates from '@features/Modulo3/jsons/Templates';
+import Template from '@features/Modulo3/components/Cards/Template/Template';
+import './Plantillas.css';
 
 const Index = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+
+	const filters = (
+		<Form>
+			<Form.Group controlId='searchTemplates' className='pl-indexFilters'>
+				<InputGroup>
+					<InputGroup.Text id='pl-indexSearch'>
+						<Search/>
+					</InputGroup.Text>
+					<Form.Control placeholder='Buscar plantilla' aria-describedby='ec-indexSearch'/>
+				</InputGroup>
+			<Button variant='primary' className='pl-indexFilterElement'>Buscar</Button>
+			</Form.Group>
+		</Form>
+	)
+
+	const templates = (
+        <div className='pl-templates'>
+                {Templates.map((template) =>{
+                    return (
+                        <div>
+                            <Template 
+                                image={template.image} 
+                                imageStyle={template.imageStyle} 
+                                name={template.name} 
+                            />
+                        </div>
+                    )              
+                })}
+        </div>
+    );
+
+	const content = (
+        <div>
+        {templates}
+        </div>
+    );
+
+    const body = (
+        <Section title={'Plantillas'} content={content} filters={filters}/>
+    );
+
+	return (
+		<div>
+			<Layout
+				title={`Plantillas de evaluación`}
+				body={body}
+				subtitle='Plantillas de evaluación continua y de desempeño'
+			/>
+		</div>
+	);
 };
+
 
 export default Index;
