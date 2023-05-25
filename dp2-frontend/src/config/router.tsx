5eaa1a65ce426ad19d422239f04ca72144a7d2e2
Edinson Ramiro Tavara Davila <etavarad@pucp.edu.pe>
2023-05-25 15:41:44 -0500

    proceso de seleccion y oferta laboral, separados

--- dp2-frontend/src/config/router.tsx
+++ dp2-frontend/src/config/router.tsx
@@ -1,116 +1,122 @@
-import { Suspense, lazy } from 'react';
-import { Navigate } from 'react-router-dom';
-import { RouteObject } from 'react-router';
+import { Suspense, lazy } from "react";
+import { Navigate } from "react-router-dom";
+import { RouteObject } from "react-router";
 
 const Loader = (Component) => (props) =>
-(
-  <Suspense fallback={<></>}>
-    <Component {...props} />
-  </Suspense>
-);
-
+    (
+        <Suspense fallback={<></>}>
+            <Component {...props} />
+        </Suspense>
+    );
 
 const Modulo1LP = Loader(
-  lazy(() => import('@features/Modulo1/pages/LearningPath'))
+    lazy(() => import("@features/Modulo1/pages/LearningPath"))
 );
 
 const M1ListLearningPath = Loader(
-  lazy(() => import('@features/Modulo1/pages/LearningPath'))
+    lazy(() => import("@features/Modulo1/pages/LearningPath"))
 );
 
 const M1LearningPathDetails = Loader(
-  lazy(() => import('@features/Modulo1/pages/LearningPath/Details'))
+    lazy(() => import("@features/Modulo1/pages/LearningPath/Details"))
 );
 
 const M1AddCourse = Loader(
-  lazy(() => import('@features/Modulo1/pages/Course/AddCourse'))
+    lazy(() => import("@features/Modulo1/pages/Course/AddCourse"))
 );
 
 const M1ListTraining = Loader(
-  lazy(() => import('@features/Modulo1/pages/Training'))
+    lazy(() => import("@features/Modulo1/pages/Training"))
 );
 
 const M1TrainingDetails = Loader(
-  lazy(() => import('@features/Modulo1/pages/Training/Details'))
+    lazy(() => import("@features/Modulo1/pages/Training/Details"))
 );
 
 const M1TrainingAssignment = Loader(
-  lazy(() => import('@features/Modulo1/pages/Training/Assignment'))
+    lazy(() => import("@features/Modulo1/pages/Training/Assignment"))
 );
 
 const ConfigSelectionProcess = Loader(
-  lazy(() => import('@features/Modulo4/pages/ConfigSelectionProcess'))
+    lazy(() => import("@features/Modulo4/pages/ConfigSelectionProcess"))
+);
+
+const ConfigProcesoSeleccion = Loader(
+    lazy(() => import("@features/Modulo4/pages/ConfigProcesoSeleccion"))
 );
 
 const ConfigOfertaLaboral = Loader(
-  lazy(() => import('@features/Modulo4/pages/ConfigOfertaLaboral'))
+    lazy(() => import("@features/Modulo4/pages/ConfigOfertaLaboral"))
 );
-
 
 /** HERE YOU DEFINE ALL THE ROUTES OF THE APP */
 
 const routes: RouteObject[] = [
-  {
-    path: 'modulo1',
-    children: [
-      {
-        path: '',
-        element: <Modulo1LP />
-      },
-      {
-        path: 'rutadeaprendizaje',
+    {
+        path: "modulo1",
         children: [
-          {
-            path: '',
-            element: <M1ListLearningPath />
-          },
-          {
-            path: 'detalle/:learningPathId',
-            element: <M1LearningPathDetails />
-          },
-        ]
-      },
-      {
-        path: 'curso',
+            {
+                path: "",
+                element: <Modulo1LP />,
+            },
+            {
+                path: "rutadeaprendizaje",
+                children: [
+                    {
+                        path: "",
+                        element: <M1ListLearningPath />,
+                    },
+                    {
+                        path: "detalle/:learningPathId",
+                        element: <M1LearningPathDetails />,
+                    },
+                ],
+            },
+            {
+                path: "curso",
+                children: [
+                    {
+                        path: "agregar/:learningPathId",
+                        element: <M1AddCourse />,
+                    },
+                ],
+            },
+            {
+                path: "capacitacion",
+                children: [
+                    {
+                        path: "",
+                        element: <M1ListTraining />,
+                    },
+                    {
+                        path: "detalle/:trainingID",
+                        element: <M1TrainingDetails />,
+                    },
+                    {
+                        path: "asignacion/:trainingID",
+                        element: <M1TrainingAssignment />,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        path: "modulo4",
         children: [
-          {
-            path: 'agregar/:learningPathId',
-            element: <M1AddCourse />
-          },
-        ]
-      },
-      {
-        path: 'capacitacion',
-        children: [
-          {
-            path: '',
-            element: <M1ListTraining />
-          },
-          {
-            path: 'detalle/:trainingID',
-            element: <M1TrainingDetails />
-          },
-          {
-            path: 'asignacion/:trainingID',
-            element: <M1TrainingAssignment />
-          },
-        ]
-      },
-    ]
-  },
-  {
-    path: 'modulo1',
-    children: [
-      {
-        path: 'configurar-proceso-seleccion',
-        element: <ConfigSelectionProcess />
-      },
-      {
-        path: 'configurar-oferta-laboral',
-        element: <ConfigOfertaLaboral />
-      },
-    ]
-  },
-]
+            {
+                path: "configurar-proceso-seleccion-old",
+                element: <ConfigSelectionProcess />,
+            },
+            {
+                path: "configurar-proceso-seleccion",
+                element: <ConfigProcesoSeleccion />,
+            },
+            {
+                path: "configurar-oferta-laboral",
+                element: <ConfigOfertaLaboral />,
+            },
+        ],
+    },
+];
 
 export default routes;
