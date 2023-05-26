7eaa9af334e52ab6c542884eef5003e1c47c6b83
joSocAngel <112914597+joSocAngel@users.noreply.github.com>
2023-05-26 07:13:07 +0200

    Update axios.ts

--- dp2-frontend/src/config/axios.ts
+++ dp2-frontend/src/config/axios.ts
@@ -7,7 +7,11 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:8000/api/';
 
 const axiosInt = axios.create({
-  baseURL: baseUrl
+  baseURL: baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token a3a137a49d06ec183aaa0bd93cabe5e5d3373257',
+  }
 });
 
 // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
