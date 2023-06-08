7373f98d81b5e64072f8fc50a26266fc73bbb614
RodrigoOrtegaBocanegra <a20180289@pucp.edu.pe>
2023-06-08 16:10:13 -0500

    Update axios.ts

--- dp2-frontend/src/config/axios.ts
+++ dp2-frontend/src/config/axios.ts
@@ -7,14 +7,22 @@ import axios from 'axios';
  const baseUrl = 'http://localhost:8000/api/';
 // const baseUrl = 'https://9dsao8d5fc.execute-api.us-east-1.amazonaws.com/dev-modulo-capacitaciones/api/';
 
+// const axiosInt = axios.create({
+//   baseURL: baseUrl,
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization': localStorage.getItem('accessToken')
+//     ? 'Token ' + localStorage.getItem('accessToken')
+//     : null,
+// }
+// });
+
 const axiosInt = axios.create({
   baseURL: baseUrl,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': localStorage.getItem('accessToken')
-    ? 'Token ' + localStorage.getItem('accessToken')
-    : null,
-}
+    'Authorization': 'Token b7f640fc5d833b0329e6b1045f088c0fcf5f191e',
+  }
 });
 
 // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
