a56f0fe883ef773ee6141dda71405bc365294b55
OlegSovero <a20173075@pucp.edu.pe>
2023-06-14 09:39:57 -0500

    agregando JSON para prueba harcode

--- dp2-frontend/src/features/Modulo2/Components/JobOpportunitiesHumanResources/JobOpportunityCandidates.tsx
+++ dp2-frontend/src/features/Modulo2/Components/JobOpportunitiesHumanResources/JobOpportunityCandidates.tsx
@@ -27,6 +27,63 @@ const returnLevel = (number) => {
   return "Muy alto"
 }
 
+
+//cabecera para la tabla
+const JSONheader = ["Nombres","Apellidos","Puesto","Area","Porcentaje de coincidencia","Correo","Estado","Activo"]
+const JSONresponse = [
+    {
+        "id": 1,
+        "user__first_name": "Diego",
+        "user__last_name": "Castro",
+        "position__name": "Analista",
+        "area__name": "Contabilidad",
+        "percentaje_conicidence" : 90,
+        "user__email": "demoemployee1@demoemployee1.com",
+        "user__is_active": true
+    },
+    {
+        "id": 2,
+        "user__first_name": "Luis",
+        "user__last_name": "Polo",
+        "position__name": "Jefe",
+        "area__name": "TI",
+        "percentaje_conicidence" : 90,
+        "user__email": "demoemployee2@demoemployee2.com",
+        "user__is_active": true
+    },
+    {
+        "id": 3,
+        "user__first_name": "Andres",
+        "user__last_name": "Vargas",
+        "position__name": "Asistente",
+        "area__name": "Contabilidad",
+        "percentaje_conicidence" : 90,
+        "user__email": "demoemployee3@demoemployee3.com",
+        "user__is_active": true
+    },
+    {
+        "id": 4,
+        "user__first_name": "Romelu",
+        "user__last_name": "Lukaku",
+        "position__name": "Analista",
+        "area__name": "TI",
+        "percentaje_conicidence" : 90,
+        "user__email": "demoemployee3@demoemployee4.com",
+        "user__is_active": true
+    },
+    {
+        "id": 5,
+        "user__first_name": "Miguel",
+        "user__last_name": "Zapata",
+        "position__name": "Asistente",
+        "area__name": "Contabilidad",
+        "percentaje_conicidence" : "90",
+        "user__email": "demoemployee3@demoemployee5.com",
+        "user__is_active": true
+    }
+
+]
+
   return (
     <div className='container'>
       <div className='row'>
--- dp2-frontend/src/features/Modulo2/Components/JobOpportunitiesRelatedToSkills/index.tsx
+++ dp2-frontend/src/features/Modulo2/Components/JobOpportunitiesRelatedToSkills/index.tsx
@@ -7,6 +7,60 @@ const JobOpportunitiesRelatedToSkills = () => {
                   {image: "imagen 2", title: "Líder técnico de arquitectura", availability: "Disponible hace 13 días", description: "Responsable de dar forma a la arquitectura planteada por el Arquitecto de software, haciendo énfasis en las especificaciones que  no se presentan en detalle.", labels: ['Liderazgo', 'Comunicación', 'Trabajo en equipo']},
                   {image: "imagen 3", title: "Analista de sistemas", availability: "Disponible hace 15 días", description: "Identificar las necesidades de los sistemas TIC de una empresa y de elaborar un proyecto que ofrezca una solución integrada.", labels: ['Análisis de sistemas', 'Innovación', 'Creatividad']}]
 
+    const JSONresponse = 
+    [
+
+        {
+          "id": 1,
+          "hiring_process": 1,
+          "introduction": "Desarrollador FrontEnd",
+          "offer_introduction": "Puesto laboral a tiempo completo para el área de TI",
+          "responsabilities_introduction": "Se encarga del componente frontend del desarrollo de aplicaciones",
+          "availability": "Disponible hace 15 días",
+          "is_active": true,
+          "photo_url": "https://imagen.com/12345/Foto1.png",
+          "location": "Sede principal",
+          "salary_range": "Entre S/.3000 y S/.4000",
+          "labels": { "label1":"Análisis de sistemas","label2": "Innovación"}
+      },
+   {
+          "id": 2,
+          "hiring_process": 2,
+          "introduction": "UX/UI Designer",
+          "offer_introduction": "Puesto laboral a tiempo completo para el área de TI",
+          "responsabilities_introduction": "Se encarga del diseño de sistemas a nivel de UX/UI",
+          "availability": "Disponible hace 9 días",
+          "is_active": true,
+          "photo_url": "https://imagen.com/12346/Foto2.png",
+          "location": "Sede principal",
+          "salary_range": "Entre S/.3000 y S/.4000",
+          "labels": {}
+      },
+       {
+          "id": 3,
+          "hiring_process": 3,
+          "introduction": "Analista de sistemas",
+          "offer_introduction": "Puesto laboral a tiempo completo para el área de TI",
+          "responsabilities_introduction": "Se encarga del diseño de gestionar los sistemas",
+          "availability": "Disponible hace 10 días",
+          "is_active": true,
+          "photo_url": "https://imagen.com/12346/Foto2.png",
+          "location": "Sede principal",
+          "salary_range": "Entre S/.3000 y S/.4000",
+          "labels": {}
+      }
+  
+  ]  
+  
+  /*
+  PAR EL API
+  Body:
+{
+        "area": 0,
+        "posicion": 0,
+        "activo": 2
+}
+*/
     return (
         <div className='container'>
             <div className='row'>
