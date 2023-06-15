9ceb44057ecafc4bc4a929395030a67c823782cb
OlegSovero <a20173075@pucp.edu.pe>
2023-06-15 14:04:29 -0500

    cambiando el app.js

--- dp2-frontend/src/App.tsx
+++ dp2-frontend/src/App.tsx
@@ -2,23 +2,14 @@ import { useRoutes } from "react-router-dom";
 import store from "@redux/store";
 import { Provider } from "react-redux";
 import router from "@routes/router";
-import GestionCompetencia from "@features/Modulo2/Components/JobStatisticsAreaManager/GestionCompetencias";
-import ConsolidadoCompetencias from "@features/Modulo2/Components/JobStatisticsAreaManager/ConsolidadoCompetencias";
-import DetalleCompetenciasArea from "@features/Modulo2/Components/JobStatisticsAreaManager/DetalleCompetenciasArea";
-
-
+import DetalleCompetenciasArea from "@features/Modulo2/Components/VisualizacionBrechasTrabajadorRRHH/DetalleCompetenciasArea";
 
 const App: React.FC = () => {
 	const content = useRoutes(router);
 
 	{
-		/*
 		return <Provider store={store}>{content}</Provider>;
-		*/
-
 	}
-
-	return <ConsolidadoCompetencias />;
 
 };
 
