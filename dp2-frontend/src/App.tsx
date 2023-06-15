6c8d47cfcbee7adb03c4d23b4d7611e16975af36
J-F-Soto <john@finnerve.com>
2023-06-15 09:37:53 -0500

    Lescano

--- dp2-frontend/src/App.tsx
+++ dp2-frontend/src/App.tsx
@@ -2,14 +2,23 @@ import { useRoutes } from "react-router-dom";
 import store from "@redux/store";
 import { Provider } from "react-redux";
 import router from "@routes/router";
-import DetalleCompetenciasArea from "@features/Modulo2/Components/VisualizacionBrechasTrabajadorRRHH/DetalleCompetenciasArea";
+import GestionCompetencia from "@features/Modulo2/Components/JobStatisticsAreaManager/GestionCompetencias";
+import ConsolidadoCompetencias from "@features/Modulo2/Components/JobStatisticsAreaManager/ConsolidadoCompetencias";
+import DetalleCompetenciasArea from "@features/Modulo2/Components/JobStatisticsAreaManager/DetalleCompetenciasArea";
+
+
 
 const App: React.FC = () => {
 	const content = useRoutes(router);
 
 	{
+		/*
 		return <Provider store={store}>{content}</Provider>;
+		*/
+
 	}
+
+	return <ConsolidadoCompetencias />;
 
 };
 
