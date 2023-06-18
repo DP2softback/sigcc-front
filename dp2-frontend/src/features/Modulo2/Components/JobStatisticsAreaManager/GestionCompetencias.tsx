64545cef595cf73035887d636e2e383f9c341a2a
OlegSovero <a20173075@pucp.edu.pe>
2023-06-17 20:24:40 -0500

    token

--- dp2-frontend/src/features/Modulo2/Components/JobStatisticsAreaManager/GestionCompetencias.tsx
+++ dp2-frontend/src/features/Modulo2/Components/JobStatisticsAreaManager/GestionCompetencias.tsx
@@ -53,7 +53,7 @@ const GestionCompetenciaAM = () => {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
-                Authorization: 'Token 5ad77c64f19039ef87cca20c2308ddbbaf3014bf',
+                Authorization: 'Token 06ef101f0752dd28182b9e8535add969ca6aa35d',
               },
               body: JSON.stringify(body),
             }
