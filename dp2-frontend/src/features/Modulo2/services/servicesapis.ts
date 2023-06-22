0b0e86c8fbcae76acee02714db6f7f3d5b5f825f
OlegSovero <a20173075@pucp.edu.pe>
2023-06-21 23:15:37 -0500

    actualizando

--- dp2-frontend/src/features/Modulo2/services/servicesapis.ts
+++
@@ -1,2 +0,0 @@
-export const URL_SERVICE = 'https://jqikkqy40h.execute-api.us-east-1.amazonaws.com/dev/api/v1'; // NOMBRE DEL MODULO 2
-export const TOKEN_SERVICE = 'Token 06ef101f0752dd28182b9e8535add969ca6aa35d';
