887df7019f8cb95c50b1f5ea302ab1db6d81016d
Edinson <etavarad@pucp.edu.pe>
2023-06-21 22:08:52 -0500

    fix apis proceso de selección

--- dp2-frontend/src/features/Modulo4/utils/constants.tsx
+++ dp2-frontend/src/features/Modulo4/utils/constants.tsx
@@ -2,16 +2,16 @@
 LOCALHOST: 9af549d853112bea2e447a0c442cf52c8af3c056
 DEPLOY: 5ad77c64f19039ef87cca20c2308ddbbaf3014bf
 */
-//export const SAMPLE_TOKEN = "9af549d853112bea2e447a0c442cf52c8af3c056";
-export const SAMPLE_TOKEN = "5ad77c64f19039ef87cca20c2308ddbbaf3014bf";
+export const SAMPLE_TOKEN = "0b13ea4764ec483c318f031f90b2b0d35ba14ddd";
+//export const SAMPLE_TOKEN = "5ad77c64f19039ef87cca20c2308ddbbaf3014bf";
 
 /* CONNECTION
 LOCALHOST: http://localhost:8000/api/v1
 DEPLOY: https://jqikkqy40h.execute-api.us-east-1.amazonaws.com/dev/api/v1
 */
-//export const LOCAL_CONNECTION = "http://localhost:8000/api/v1";
-export const LOCAL_CONNECTION =
-	"https://jqikkqy40h.execute-api.us-east-1.amazonaws.com/dev/api/v1";
+export const LOCAL_CONNECTION = "http://localhost:8000/api/v1";
+//export const LOCAL_CONNECTION =
+//	"https://jqikkqy40h.execute-api.us-east-1.amazonaws.com/dev/api/v1";
 
 // PROCESO SELECCION
 export const GET_POSICIONES_TRABAJO = "/areaxposition";
