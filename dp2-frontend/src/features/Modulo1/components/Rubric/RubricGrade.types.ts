169bf711baa439cc5e2d4e50a599a03a45cd37dc
joSocAngel <112914597+joSocAngel@users.noreply.github.com>
2023-06-22 20:54:21 +0200

    Rúbrica de calificación

--- dp2-frontend/src/features/Modulo1/components/Rubric/RubricGrade.types.ts
+++ dp2-frontend/src/features/Modulo1/components/Rubric/RubricGrade.types.ts
@@ -1,17 +1,29 @@
-export type Props = {
-    disabled: boolean,
-    rubric: {
-        criterias: Array<
-            {
-                label: string,
-                score: number
-            }
-        >,
-        studentScore: number,
-        score: number,
-    },
+export type Level = {
+    id: number,
+    name: string,
 }
 
-export type State = {
-    score: number,
+export type Criteria = {
+    id: number,
+    name: string,
+    level: Level,
+}
+
+export type PropsRubricCriterias = {
+    criterias: Array<Criteria>,
+    disabled: boolean
+}
+
+export type StateRubricCriterias = {
+    criterias: Array<Criteria>
+}
+
+export type PropsChoiceBase = {
+    name: string,
+    choice: Level,
+    onChange: any,
+    disabled: boolean,
+}
+
+export type StateChoiceBase = {
 }
