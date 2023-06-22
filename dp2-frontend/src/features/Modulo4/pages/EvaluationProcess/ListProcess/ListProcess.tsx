16196126ef045c8448f572979b9b690dabaefee7
Fabricio Sebastian Davila Diaz <70768874+sebas09-gcc@users.noreply.github.com>
2023-06-22 18:58:33 -0500

    Add StepList

--- dp2-frontend/src/features/Modulo4/pages/EvaluationProcess/ListProcess/ListProcess.tsx
+++ dp2-frontend/src/features/Modulo4/pages/EvaluationProcess/ListProcess/ListProcess.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Table, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 interface Process {
   id: number;
   name: string;
-  status: string;
+  position: string;
+  area: string;
+  currentStage: number;
 }
 
 const testData: Process[] = [
-  { id: 1, name: 'Proceso de selección 1', status: 'Completado' },
-  { id: 2, name: 'Proceso de selección 2', status: 'En progreso' },
-  { id: 3, name: 'Proceso de selección 3', status: 'Completado' },
-  { id: 4, name: 'Proceso de selección 4', status: 'Pendiente' },
-  { id: 5, name: 'Proceso de selección 5', status: 'En progreso' },
+  { id: 1, name: 'Entrevista inicial con candidato A', position: 'Desarrollador Frontend', area: 'Tecnología', currentStage: 3 },
+  { id: 2, name: 'Evaluación técnica del candidato B', position: 'Desarrollador Backend', area: 'Tecnología', currentStage: 2 },
+  { id: 3, name: 'Evaluación de habilidades blandas del candidato C', position: 'Gerente de Proyectos', area: 'Gestión', currentStage: 4 },
+  { id: 4, name: 'Entrevista final con el candidato D', position: 'Diseñador UX/UI', area: 'Diseño', currentStage: 1 },
+  { id: 5, name: 'Revisión de referencias del candidato E', position: 'Especialista en Marketing Digital', area: 'Marketing', currentStage: 2 },
 ];
 
 const TableComponent: React.FC = () => {
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = React.useState('');
+  const navigate = useNavigate();
 
   const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilter(event.target.value);
@@ -25,6 +29,13 @@ const TableComponent: React.FC = () => {
   const filteredData = testData.filter((process) =>
     process.name.toLowerCase().includes(filter.toLowerCase())
   );
+
+  const handleRowClick = (process: Process) => {
+    // Aquí puedes establecer la ruta a la que deseas redirigir
+    localStorage.setItem("step",process.currentStage.toString())
+    navigate(`/selection-offers-and-positions/selection-process/step/`);
+    
+  };
 
   return (
     <div>
@@ -43,15 +54,19 @@ const TableComponent: React.FC = () => {
           <tr>
             <th>ID</th>
             <th>Nombre</th>
-            <th>Estado</th>
+            <th>Posición</th>
+            <th>Área</th>
+            <th>Etapa Actual</th>
           </tr>
         </thead>
         <tbody>
           {filteredData.map((process) => (
-            <tr key={process.id}>
+            <tr key={process.id} onClick={() => handleRowClick(process)}>
               <td>{process.id}</td>
               <td>{process.name}</td>
-              <td>{process.status}</td>
+              <td>{process.position}</td>
+              <td>{process.area}</td>
+              <td>{process.currentStage}</td>
             </tr>
           ))}
         </tbody>
--- dp2-frontend/src/features/Modulo4/routes/router.tsx
+++ dp2-frontend/src/features/Modulo4/routes/router.tsx
@@ -14,7 +14,8 @@ import {
 	CREATE_SELECTION_PROCESS,
 	LIST_SELECTION_PROCESS,
 	DETAIL_JOB_OFFER,
-	REGISTER_IN_JOB_OFFER
+	REGISTER_IN_JOB_OFFER,
+	LIST_SELECTION_PROCESS_STEP
 } from "./path";
 
 const Loader = (Component) => (props) =>
@@ -58,6 +59,10 @@ const RegisterInOferta = Loader(
 
 const ListProcess = Loader(
 	lazy(() => import("@features/Modulo4/pages/EvaluationProcess/ListProcess/ListProcess"))
+);
+
+const ListStep = Loader(
+	lazy(() => import("@features/Modulo4/pages/EvaluationProcess/ListProcess/ListSteps"))
 );
 
 export const routes: RouteObject[] = [
@@ -157,6 +162,20 @@ export const routes: RouteObject[] = [
 									Roles.CANDIDATE
 								]}>
 								<ListProcess/>
+							</AppLayout>
+						)
+					},
+					{
+						path: LIST_SELECTION_PROCESS_STEP,
+						element: (
+							<AppLayout
+								allowedRoles={[
+									Roles.HR_ADMIN,
+									Roles.HR_MANAGER,
+									Roles.HR_WORKER,
+									Roles.CANDIDATE
+								]}>
+								<ListStep/>
 							</AppLayout>
 						)
 					},
