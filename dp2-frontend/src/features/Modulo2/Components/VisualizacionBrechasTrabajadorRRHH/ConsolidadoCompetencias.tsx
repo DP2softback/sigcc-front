cc2f4bc47cd2bec4273e063b919bcdfb165144ef
OlegSovero <a20173075@pucp.edu.pe>
2023-06-23 22:59:03 -0500

    quitando botones

--- dp2-frontend/src/features/Modulo2/Components/VisualizacionBrechasTrabajadorRRHH/ConsolidadoCompetencias.tsx
+++ dp2-frontend/src/features/Modulo2/Components/VisualizacionBrechasTrabajadorRRHH/ConsolidadoCompetencias.tsx
@@ -212,7 +212,7 @@ const PieChart = ({ title, labels, datasets }) => {
                     <div className="chart-legend"> 
                       {/* Agregar aquí la leyenda del gráfico 1 */}
                     </div>
-                    <button className="btn btn-secondary" onClick={handleMostrarLineChartClick}>Mostrar en linechart</button>
+
                   </div>
                 </div>
               </div>
@@ -225,7 +225,7 @@ const PieChart = ({ title, labels, datasets }) => {
                    <div className="chart-legend">
                      {/* Agregar aquí la leyenda del gráfico 2 */}
                    </div>
-                   <button className="btn btn-secondary" onClick={handleMostrarLineChartClick}>Mostrar en linechart</button>
+
                    <button className="btn btn-secondary" onClick={handleClick}>Ver detalle del área</button>
                    </div>
                </div>
