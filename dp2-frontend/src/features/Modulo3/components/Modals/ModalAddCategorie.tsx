8196ecc4693fdc4235466aaac9209c187b1c3d0a
FaridFranz <farid@MSI>
2023-07-12 00:15:40 -0500

    validaciones

--- dp2-frontend/src/features/Modulo3/components/Modals/ModalAddCategorie.tsx
+++ dp2-frontend/src/features/Modulo3/components/Modals/ModalAddCategorie.tsx
@@ -7,7 +7,6 @@ import { agregarCategoria, listarCompetenciasFree } from "@features/Modulo3/serv
 import { ToastContainer, toast } from "react-toastify";
 import { PlusCircle } from "react-bootstrap-icons";
 
-const competenciasIni = ["Competencia 1", "Competencia 2", "Competencia 3"]
 const ModalAddCategorie = (props) => {
   const { show, setShow } = props;
   const handleClose = () => {
@@ -20,6 +19,8 @@ const ModalAddCategorie = (props) => {
   const [competencias, setCompetencias] = useState([]);
   const [selectedId, setSelectedId] = useState(-1);
   const [isLoading, setIsLoading] = useState(true);
+  const [dropdown,setDropdown] = useState(false);
+  const [formError, setFormError] = useState(false); 
 
   useEffect(() => {
     setIsLoading(true);
@@ -37,24 +38,38 @@ const ModalAddCategorie = (props) => {
       ? 'bg-blue'
       : 'bg-white';
   }
-  const handleAgregar = () => {
-    const newSubcategory = {
-      name: subcategoriaName,
-      description: "",
-    };
 
-    subcategorias.push(newSubcategory)
-    setSubcategoriaName("");
-    setSelectedOption('');
+  const handleAgregar = () => {
+    if (subcategoriaName.trim() !== '') {
+      const newSubcategory = {
+        name: subcategoriaName,
+        description: "",
+      };
+
+      subcategorias.push(newSubcategory)
+      setSubcategoriaName("");
+      setSelectedOption('');
+      setFormError(false); 
+    } else {
+      setFormError(true); 
+    }
   };
   const handleAgregarExistente = () => {
-    const newSubcategory = {
-      id: selectedId,
-    };
+    if (selectedOption !== '') {
+      const newSubcategory = {
+        name: selectedOption,
+        id: selectedId,
+      };
 
-    subcategorias.push(newSubcategory)
-    setSubcategoriaName("");
-    setSelectedOption('');
+      subcategorias.push(newSubcategory)
+      setSubcategoriaName("");
+      setSelectedOption('');
+      const updatedCompetencias = competencias.filter((competencia) => competencia.name !== selectedOption);
+      setCompetencias(updatedCompetencias);
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
   };
 
   const handleEliminar = (nombre: string) => {
@@ -67,11 +82,12 @@ const ModalAddCategorie = (props) => {
     setSelectedOption(option);
     setSelectedId(selectedCompetencia.id);
   };
+
   const handleChangeSubcategoriaName = (event) => {
     setSubcategoriaName(event.target.value);
   };
 
-  const handleChangeCategoriaName = (event) => {
+  const handleChangeCategoriaName = (event) => { 
     setCategoriaName(event.target.value);
   };
 
@@ -121,7 +137,7 @@ const ModalAddCategorie = (props) => {
           </div>
         </div>
       </Form.Group>
-      <label className="label-estilizado" style={{ marginTop: "20px",marginBottom: "10px" }} htmlFor="nombreSubcategoría">
+      <label className="label-estilizado" style={{ marginTop: "20px", marginBottom: "10px" }} htmlFor="nombreSubcategoría">
         A continuación, cree las competencias que pertenecerán a la nueva categoría
       </label>
       <Form.Group>
@@ -135,34 +151,45 @@ const ModalAddCategorie = (props) => {
               id="nombreSubcategoría"
               value={subcategoriaName}
               onChange={handleChangeSubcategoriaName}
+              isInvalid={formError} 
             />
+            {formError && <Form.Control.Feedback type="invalid" className="input-feedback">Ingrese un nombre de competencia válido.</Form.Control.Feedback>}
+
             <Button variant="outline-primary" onClick={handleAgregar} className="ca-buttonAdd">
               +
             </Button>
           </div>
         </div>
       </Form.Group>
-      {/* <Form.Group>
+      <Form.Group>
         <div className="label-input-container">
           <label className="label-estilizado" htmlFor="dropdown">
-            Competencia existente
+            Competencias existentes
           </label>
           <div className="input-button-container">
-            <Form.Select
-              value={selectedOption}
-              onChange={(e) => handleOptionSelect(e.target.value)}
-            >
-              <option hidden>Seleccione una competencia</option>
-              {competencias.map((competencia, index) => {
-                return <option key={competencia.id}> {competencia.name} </option>;
-              })}
+            <Form.Select value={selectedOption} onChange={(e) => handleOptionSelect(e.target.value)} disabled={competencias.length === 0} isInvalid={dropdown}>
+              {competencias.length === 0 && (
+              <option hidden>No hay competencias libres</option>
+              )}
+              {competencias.length > 0 && (
+                <>
+                <option hidden>Seleccione una competencia</option>
+                {competencias.map((competencia, index) => {
+                  return <option key={competencia.id}> {competencia.name} </option>;
+                })}
+                </>)}
             </Form.Select>
-            <Button variant="outline-primary" onClick={handleAgregarExistente} className="ca-buttonAdd">
+            {dropdown  && (
+              <Form.Control.Feedback type="invalid" className="input-feedback">
+                Seleccione una competencia.
+              </Form.Control.Feedback>
+            )}
+            <Button variant="outline-primary" onClick={handleAgregarExistente} className="ca-buttonAdd" disabled={competencias.length === 0}>
               +
             </Button>
           </div>
         </div>
-      </Form.Group> */}
+      </Form.Group>
     </Form>
   );
 
@@ -173,31 +200,49 @@ const ModalAddCategorie = (props) => {
       setTimeout(resolve, ms);
     });
   }
+
   const closeNotification = async () => {
     await delay(4000);
     window.location.reload();
   };
+
+  const closeNotificationError = async () => {
+    await delay(4000);
+  };
+
   const handleGuardar = () => {
     console.log(subcategorias);
-    const data = {nombre: categoriaName, Subcategorias: subcategorias};
-    (async () => {
-      const response = await agregarCategoria(data);
-      if (response) {
-        toast.success("Se ha añadido correctamente la categoría con sus competencias");
-        setShow(false);
-        closeNotification();
+    const data = { nombre: categoriaName, Subcategorias: subcategorias };
+    if(categoriaName!=''){
+      if (subcategorias.length > 0) {
+        (async () => {
+          const response = await agregarCategoria(data);
+          if (response) {
+            toast.success("Se ha añadido correctamente la categoría con sus competencias");
+            setShow(false);
+            closeNotification();
+          }
+        })();
+      }else{
+        toast.error("Debe asignar al menos una competencia a la nueva categoría");
+        //setShow(false);
+        closeNotificationError();
       }
-    })();
+    }else{
+      toast.error("Debe asignar un nombre a la nueva categoría");
+      //setShow(false);
+      closeNotificationError();
+    }
   }
   return (
     <Modal show={show} onHide={handleClose}>
       <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
 
       <Modal.Header closeButton>
-        <Modal.Title>Agregar Competencia</Modal.Title>
+        <Modal.Title>Agregar Categoría</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div>          
+        <div>
           {subcategoriasForm}
           {table}
         </div>
@@ -206,7 +251,7 @@ const ModalAddCategorie = (props) => {
         <Button variant='outline-primary' className='boton-dejar mr-10' onClick={() => setShow(false)}>
           Volver
         </Button>
-        <Button variant="primary" onClick={handleGuardar}>
+        <Button variant="primary" onClick={handleGuardar} disabled={subcategorias.length === 0 || categoriaName===''} >
           Agregar
         </Button>
       </Modal.Footer>
