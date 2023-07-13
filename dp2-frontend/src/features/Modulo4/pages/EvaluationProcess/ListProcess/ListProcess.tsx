cf8a6ba4890d744c94fb43c8eab4d7ec4cf4ea33
Fabricio Sebastian Davila Diaz <70768874+sebas09-gcc@users.noreply.github.com>
2023-07-13 12:03:09 -0500

    .

--- dp2-frontend/src/features/Modulo4/pages/EvaluationProcess/ListProcess/ListProcess.tsx
+++ dp2-frontend/src/features/Modulo4/pages/EvaluationProcess/ListProcess/ListProcess.tsx
@@ -1,38 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import {LOCAL_CONNECTION, SAMPLE_TOKEN}  from '../../../utils/constants';
+import axios from 'axios';
 
 interface Process {
   id: number;
-  position: string;
-  area: string;
-  currentStage: number;
-  vacancies: number;
+  start_date: string;
+  end_date: string;
+  order: number;
+  name: string;
+  description: string;
+  creation_date: string;
+  modified_date: string;
+  is_active: boolean;
+  position_similarity: string | null;
+  hiring_process: number;
+  areaxpositiondetail: Area;
+  current_process_stage: Stage;
+  available_positions_quantity: string
 }
 
-const testData: Process[] = [
-  { id: 1, position: 'Desarrollador Frontend', area: 'Tecnología', currentStage: 3, vacancies: 2 },
-  { id: 2, position: 'Desarrollador Backend', area: 'Tecnología', currentStage: 2, vacancies: 1 },
-  { id: 3, position: 'Gerente de Proyectos', area: 'Gestión', currentStage: 4, vacancies: 3 },
-  { id: 4, position: 'Diseñador UX/UI', area: 'Diseño', currentStage: 1, vacancies: 1 },
-  { id: 5, position: 'Especialista en Marketing Digital', area: 'Marketing', currentStage: 2, vacancies: 2 },
-];
+interface Area {
+  area_name: string;
+}
+
+interface Stage {
+  name: string;
+  stage_type: string;
+}
 
 const TableComponent: React.FC = () => {
-  const [filter, setFilter] = React.useState('');
+  const [filter, setFilter] = useState('');
+  const [processes, setProcesses] = useState<Process[]>([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchProcesses();
+  }, []);
+
+  const fetchProcesses = async () => {
+    try {
+      const response = await axios.get(`${LOCAL_CONNECTION}/hiring-processes`,{
+      headers: {
+        Authorization: `Token ${SAMPLE_TOKEN}`
+      }});
+      const data = response.data;
+      setProcesses(data);
+    } catch (error) {
+      console.error('Error fetching processes:', error);
+    }
+  };
 
   const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilter(event.target.value);
   };
 
-  const filteredData = testData.filter((process) =>
-    process.position.toLowerCase().includes(filter.toLowerCase())
+  const filteredData = processes.filter((process) =>
+    process.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   const handleRowClick = (process: Process) => {
-    localStorage.setItem('step', process.currentStage.toString());
-    navigate(`/selection-offers-and-positions/selection-process/step`+process.currentStage.toString()+`/`);
+    localStorage.setItem('step', process.current_process_stage.stage_type);
+    var value = process.current_process_stage.stage_type;
+    navigate(`/selection-offers-and-positions/selection-process/step`+value+`/`);
   };
 
   return (
@@ -61,10 +92,10 @@ const TableComponent: React.FC = () => {
           {filteredData.map((process) => (
             <tr key={process.id} onClick={() => handleRowClick(process)}>
               <td>{process.id}</td>
-              <td>{process.position}</td>
-              <td>{process.area}</td>
-              <td>{process.currentStage}</td>
-              <td>{process.vacancies}</td>
+              <td>{process.name}</td>
+              <td>{process.areaxpositiondetail.area_name}</td>
+              <td>{process.current_process_stage.name}</td>
+              <td>{process.available_positions_quantity}</td>
             </tr>
           ))}
         </tbody>
--- dp2-frontend/src/features/Modulo4/pages/EvaluationProcess/ListProcess/ListSteps1.tsx
+++ dp2-frontend/src/features/Modulo4/pages/EvaluationProcess/ListProcess/ListSteps1.tsx
@@ -1,139 +1,184 @@
 import { navigateBack } from "@features/Modulo3/utils/functions";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Form, Table, Button, Row, Col } from "react-bootstrap";
+import {LOCAL_CONNECTION, SAMPLE_TOKEN}  from '../../../utils/constants';
+import axios from 'axios';
+import MultiSelect from "@features/Modulo4/components/MultiSelect";
+
 
 interface Applicant {
-	firstName: string;
-	lastName: string;
-	percentage: number;
+  
+  affinity: string;
+	pass: string;
+	score: string;
+	applicant: App;
+}
+
+interface App{
+	user: User
+}
+interface User {
+	first_name: string;
+  last_name: string;
+}
+interface Training{
+	id: number
 }
 
 const ApplicantList: React.FC = () => {
-	const [applicants, setApplicants] = useState<Applicant[]>([
-		{ firstName: "John", lastName: "97%", percentage: 80 },
-		{ firstName: "Jane", lastName: "97%", percentage: 95 },
-		{ firstName: "Michael", lastName: "97%", percentage: 70 },
-		{ firstName: "Emily", lastName: "97%", percentage: 85 },
-		{ firstName: "John", lastName: "97%", percentage: 80 },
-		{ firstName: "Jane", lastName: "97%", percentage: 95 },
-		{ firstName: "Michael", lastName: "97%", percentage: 70 },
-		{ firstName: "Emily", lastName: "97%", percentage: 85 },
-		{ firstName: "John", lastName: "97%", percentage: 80 },
-		{ firstName: "Jane", lastName: "97%", percentage: 95 },
-		{ firstName: "Michael", lastName: "97%", percentage: 70 },
-		{ firstName: "Emily", lastName: "97%", percentage: 85 },
-		{ firstName: "John", lastName: "97%", percentage: 80 },
-		{ firstName: "Jane", lastName: "97%", percentage: 95 },
-		{ firstName: "Michael", lastName: "97%", percentage: 70 },
-		{ firstName: "Emily", lastName: "97%", percentage: 85 }
-	]);
-	const [nameFilter, setNameFilter] = useState("");
-	const [percentageFilter, setPercentageFilter] = useState("");
+  const [applicants, setApplicants] = useState<Applicant[]>([]);
+  const [nameFilter, setNameFilter] = useState("");
+  const [percentageFilter, setPercentageFilter] = useState("");
+	const [training, setTraining] = useState();
+	const [loading, setLoading] = useState(true);
 
-	// Función para filtrar los postulantes
-	const filterApplicants = () => {
-		return applicants.filter((applicant) => {
-			const fullName =
-				`${applicant.firstName} ${applicant.lastName}`.toLowerCase();
-			const filteredByName = nameFilter
-				? fullName.includes(nameFilter.toLowerCase())
-				: true;
-			const filteredByPercentage = percentageFilter
-				? applicant.percentage.toString() >= percentageFilter
-				: true;
-			return filteredByName && filteredByPercentage;
+  // Función para filtrar los postulantes
+  
+
+  // Función para manejar cambios en el filtro de nombre
+  const handleNameFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNameFilter(event.target.value);
+  };
+
+  // Función para manejar cambios en el filtro de porcentaje
+  const handlePercentageFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPercentageFilter(event.target.value);
+  };
+
+  // Función para manejar el clic en el botón de filtrar
+  const handleFilterButtonClick = () => {
+		console.log("Hola")
+		console.log("afinidad:",percentageFilter)
+		const json = {
+			"hiring_process":1,    
+			"affinity":parseInt(percentageFilter)
+		}
+		axios.post(`${LOCAL_CONNECTION}/filter-second-step`, json, {
+			headers: {
+				Authorization: `Token ${SAMPLE_TOKEN}`
+			}
+		}).then((response) => {
+			const data = response.data;
+			console.log(data)
+			setApplicants(data);
+		})
+		.catch((error) => {
+			console.log(error);
 		});
-	};
+	
+  };
 
-	// Función para manejar cambios en el filtro de nombre
-	const handleNameFilterChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		setNameFilter(event.target.value);
-	};
+  const handleFilterSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+  };
 
-	// Función para manejar cambios en el filtro de porcentaje
-	const handlePercentageFilterChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		setPercentageFilter(event.target.value);
-	};
+  const handleCloseStageButtonClick = () => {
+    navigateBack();
+  };
 
-	// Función para manejar el clic en el botón de filtrar
-	const handleFilterButtonClick = () => {
-		const filteredApplicants = filterApplicants();
-		// Realizar acciones adicionales con los resultados filtrados si es necesario
-	};
+	const fetchData = async () => {
+		const json = {
+			"hiring_process":1,
+			"mandatory":[11],
+			"affinity":60
+		}
+		
+			const response = await axios.post(`${LOCAL_CONNECTION}/dummy-first-step`, json, {
+				headers: {
+					Authorization: `Token ${SAMPLE_TOKEN}`
+				}
+			}).then((response) => {
+        const data = response.data;
+			  setApplicants(data);
+				axios.get(`${LOCAL_CONNECTION}/training`,{
+					headers: {
+						Authorization: `Token ${SAMPLE_TOKEN}`
+					}}).then((response)=> {
+						setTraining(response.data);
+						console.log(response.data)
+						setLoading(false);
+					})
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+	
+}
 
-	const handleFilterSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		filterApplicants();
-	};
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-	const handleCloseStageButtonClick = () => {
-		navigateBack();
-	};
+	const handleSelectCompetencias = (ids: number[]) => {
 
-	return (
-		<Container>
+	}
+
+  return (
+    <Container>
+      {!loading && 
+			<>
 			<h1 className="mt-3">Filtrado automático</h1>
-			<Row>
-				<Col xs={12} md={9}>
-					<Table striped bordered>
-						<thead>
-							<tr>
-								<th>Nombre</th>
-								<th>Afinidad</th>
-								<th>Score</th>
-							</tr>
-						</thead>
-						<tbody>
-							{filterApplicants().map((applicant, index) => (
-								<tr key={index}>
-									<td>{applicant.firstName}</td>
-									<td>{applicant.lastName}</td>
-									<td>{applicant.percentage}</td>
-								</tr>
-							))}
-						</tbody>
-					</Table>
-				</Col>
-				<Col xs={12} md={3}>
-					<Form onSubmit={handleFilterSubmit}>
-						<Form.Control
-							type="text"
-							placeholder="Nivel de afinidad con la posición"
-							value={percentageFilter}
-							onChange={handlePercentageFilterChange}
-							className="mt-2"
+      <Row>
+        <Col xs={12} md={9}>
+          <Table striped bordered>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Afinidad</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {applicants.map((applicant, index) => (
+                <tr key={index}>
+                  <td>{applicant.applicant.user.first_name + " " +  applicant.applicant.user.last_name }</td>
+                  <td>{applicant.affinity + " " + applicant.pass}</td>
+                  <td>{applicant.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+        <Col xs={12} md={3}>
+          <Form onSubmit={handleFilterSubmit}>
+            <Form.Control
+              type="text"
+              placeholder="Nivel de afinidad con la posición"
+              value={percentageFilter}
+              onChange={handlePercentageFilterChange}
+              className="mt-2"
+            />
+            <MultiSelect
+							options={training}
+							label="name"
+							value="id"
+							placeholder="Seleccionar Competencias"
+							handleSelect={handleSelectCompetencias}
 						/>
-						<Form.Control
-							type="text"
-							placeholder="Competencia"
-							value={nameFilter}
-							onChange={handleNameFilterChange}
-							className="mt-2"
-						/>
-						<div style={{ textAlign: "right" }}>
-							<Button
-								variant="primary"
-								type="submit"
-								onClick={handleFilterButtonClick}
-								className="mt-2"
-								style={{ float: "right" }}>
-								Filtrar
-							</Button>
-						</div>
-					</Form>
-				</Col>
-			</Row>
-			<div className="d-flex justify-content-end">
-				<Button variant="danger" onClick={handleCloseStageButtonClick}>
-					Cerrar Etapa
-				</Button>
-			</div>
-		</Container>
-	);
+            <div style={{ textAlign: "right" }}>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={handleFilterButtonClick}
+                className="mt-2"
+                style={{ float: "right" }}
+              >
+                Filtrar
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+      <div className="d-flex justify-content-end">
+        <Button variant="danger" onClick={handleCloseStageButtonClick}>
+          Cerrar Etapa
+        </Button>
+      </div>
+			</>
+			}
+    </Container>
+  );
 };
 
 export default ApplicantList;
